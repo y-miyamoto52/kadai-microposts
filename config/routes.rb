cdec39e5554d76f4cab0152f8d0a437c2e81a8b8
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-25 00:31:39 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #newのurlをsignupにする
+  #resourcesに:newも追加しているので/users/newでも新規登録ページが表示される
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
