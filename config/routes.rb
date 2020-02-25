db38ff97be731b6e49d5ae44f0747dc77a82a6eb
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-25 10:58:59 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   #newのurlをsignupにする
   #resourcesに:newも追加しているので/users/newでも新規登録ページが表示される
   get 'signup', to: 'users#new'
